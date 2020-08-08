2ca3768a2df678ddffdeb0a36c991d011940df48
Emygdio Barreto <emygdio.barreto@gmail.com>
2020-08-08 11:36:36 -0300

    Validação customizada: email não repetido na atualização de Cliente

---
+++ src/main/java/br/com/its/cursomc/manager/validation/ClienteUpdateValidator.java
@@ -0,0 +1,52 @@
+package br.com.its.cursomc.manager.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import br.com.its.cursomc.dao.ClienteDao;
+import br.com.its.cursomc.domain.Cliente;
+import br.com.its.cursomc.domain.enums.TipoCliente;
+import br.com.its.cursomc.dto.ClienteDTO;
+import br.com.its.cursomc.dto.NovoClienteDto;
+import br.com.its.cursomc.manager.validation.utils.BR;
+import br.com.its.cursomc.resources.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteDao dao;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) { }
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> lista = new ArrayList<>();
+		
+		Cliente cli = dao.findByEmail(objDto.getEmail());
+		if (cli != null && !cli.getId().equals(uriId)) {
+			lista.add(new FieldMessage("email", "Email já existente"));
+		}
+		
+		for (FieldMessage field : lista) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(field.getMessage()).addPropertyNode(field.getFieldName()).addConstraintViolation();
+		}
+		return lista.isEmpty();
+	}
+}
