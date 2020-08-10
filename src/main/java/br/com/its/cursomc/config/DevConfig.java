767055c55328a56076118128060a2b9ffa66a614
Emygdio Barreto <emygdio.barreto@gmail.com>
2020-08-10 09:24:34 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/br/com/its/cursomc/config/DevConfig.java
@@ -0,0 +1,31 @@
+package br.com.its.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.its.cursomc.manager.DBManager;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBManager dbManager;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		dbManager.instantiateTestDatabase();
+		return true;
+	}
+}
