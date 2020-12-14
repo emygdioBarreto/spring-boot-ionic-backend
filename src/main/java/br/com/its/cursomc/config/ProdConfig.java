09157e633f70ec40947c515578e7f81ca8736d89
Emygdio Barreto <emygdio.barreto@gmail.com>
2020-12-14 10:14:58 -0300

    Documentacao com Swagger-Parte 1

---
+++ src/main/java/br/com/its/cursomc/config/ProdConfig.java
@@ -0,0 +1,38 @@
+package br.com.its.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.its.cursomc.manager.DBManager;
+import br.com.its.cursomc.manager.EmailManager;
+import br.com.its.cursomc.manager.SmtpEmailManager;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+
+	@Autowired
+	private DBManager dbManager;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		dbManager.instantiateTestDatabase();
+		return true;
+	}
+	
+	@Bean
+	public EmailManager emailManager() {
+		return new SmtpEmailManager();
+	}
+}
